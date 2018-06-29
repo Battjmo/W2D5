691dda8e6f885c3a6ffa1957ae8ce40511c25b48
StevenChen <steven.chen313@gmail.com>
2018-06-29 14:21:31 -0700

    Create intset, resizingintset, maxintset

---
+++ Hash_exercises/lib/p01_int_set.rb
@@ -0,0 +1,111 @@
+class MaxIntSet
+
+  attr_reader :max
+  def initialize(max)
+    @max = max
+    @store = Array.new(@max) {|el| el = false}
+
+  end
+
+  def insert(num)
+    raise "Out of bounds" if num > max || num < 0
+    @store[num] = true
+    true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+
+  attr_reader :store
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    return false if self.include?(num)
+
+    self[num] << num
+    @count += 1
+    resize! if num_buckets < @count
+
+  end
+
+  def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_array = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_array.flatten.each { |num| insert(num) }
+  end
+
+
+end
