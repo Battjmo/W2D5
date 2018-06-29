b2e479b3a91900dd193de5ffc5a0e71204d20360
StevenChen <steven.chen313@gmail.com>
2018-06-29 16:47:01 -0700

    Link Lists

--- Hash_exercises/lib/p03_hash_set.rb
+++ Hash_exercises/lib/p03_hash_set.rb
@@ -7,18 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    return false if self.include?(key)
+    self[key.hash] << key
+    @count += 1
+    resize! if count >= num_buckets
+    true
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,9 @@ class HashSet
   end
 
   def resize!
+    old_array = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_array.flatten.each { |num| insert(num) }
   end
 end
